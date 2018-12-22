5c49d22c240d68b456b4f54df605553251fca058
xwen <xwen@xwen.ca>
2018-12-22 13:18:21 -0800

    Add MobX

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world!!!");
+import { observable, computed } from "mobx"
+
+class Test {
+    @observable A = 0;
+
+    @computed get B() {
+        return this.A + 10;
+    }
+
+    @computed get C() {
+        return this.B + 20;
+    }
+}
+
+var test = new Test();
+
+console.log("C = " + test.C);
+console.log("Set A = 100");
+test.A = 100;
+console.log("C = " + test.C);
